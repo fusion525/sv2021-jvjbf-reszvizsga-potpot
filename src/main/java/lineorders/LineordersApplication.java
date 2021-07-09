67a4ff6b33a050a03b219a54de73c18d1ccdf2ae
fusion525 <fusion525@users.noreply.github.com>
2021-07-09 13:59:42 +0200

    Initial commit

---
+++ src/main/java/lineorders/LineordersApplication.java
@@ -0,0 +1,13 @@
+package lineorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineordersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineordersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lineorders/LineordersApplicationTests.java
@@ -0,0 +1,13 @@
+package lineorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineordersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
